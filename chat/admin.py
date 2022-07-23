926ae112edb73bc0a3d738db727588e4210d245e
xAlpaca <wpc@DESKTOP-GEGH2KM.localdomain>
2022-07-23 22:00:06 +0200

    SSChatV2 no old files

--- chat/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import  message, chats, chat_messages
-
-# Register your models here.
-
-admin.site.register(message)
-admin.site.register(chats)
-admin.site.register(chat_messages)
